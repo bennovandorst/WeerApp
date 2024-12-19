4273ad3dd5b86bdabf9d0f2e7f308950ddbfbafa
bennovandorst <9024828@student.zadkine.nl>
2024-12-19 13:20:03 +0100

    import fix

--- src/screens/Location.jsx
+++ src/screens/Location.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet, FlatList } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import GetLocation from 'react-native-get-location';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Weather from '../components/Weather';
